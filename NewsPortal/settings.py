e6648b0276fa0942b3330b1533ca0de3040106be
Aakash Basnet <iamaakashbasnet@gmail.com>
2021-04-01 14:59:26 +0545

    Accounts app

--- NewsPortal/settings.py
+++ NewsPortal/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'NewsPortal.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +120,28 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+
+# User uploaded files
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+
+
+# Custom User Model
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
+
+# Django Crispy Forms
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+
+# Login config
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'dashboard'
