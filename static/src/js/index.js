3ff2f1ac221141c28a9290b161ce57a1640dfb24
Aakash Basnet <iamaakashbasnet@gmail.com>
2021-04-02 10:55:29 +0545

    Chart in dashboard

--- static/src/js/index.js
+++ static/src/js/index.js
@@ -1,6 +1,5 @@
 import './../scss/main.scss';
 import 'bootstrap/dist/js/bootstrap.bundle';
-import 'chart.js';
 
 document.querySelector('#menu-toggle').addEventListener('click', (e) => {
   e.preventDefault();
