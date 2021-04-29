0f3b9b480ef43906509931e336234c580b9df44e
Aakash Basnet <iamaakashbasnet@gmail.com>
2021-04-29 16:25:00 +0545

    Made home page layout

--- static/src/js/index.js
+++ static/src/js/index.js
@@ -1,7 +1,9 @@
 import './../scss/main.scss';
 import 'bootstrap/dist/js/bootstrap.bundle';
 
-document.querySelector('#menu-toggle').addEventListener('click', (e) => {
-  e.preventDefault();
-  document.querySelector('#wrapper').classList.toggle('toggled');
-});
+if (!!document.querySelector('#menu-toggle')) {
+  document.querySelector('#menu-toggle').addEventListener('click', (e) => {
+    e.preventDefault();
+    document.querySelector('#wrapper').classList.toggle('toggled');
+  });
+}
